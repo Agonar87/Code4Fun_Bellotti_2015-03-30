f8386d2e0cd6cd86bd3986d75e092bfefcfffae9
Agonar87 <ste.bellotti@gmail.com>
2015-03-30 14:33:58 +0200

    PrintSpiral method

--- Code4Fun_Bellotti/Methods.cs
+++ Code4Fun_Bellotti/Methods.cs
@@ -11,7 +11,7 @@ namespace Code4Fun_Bellotti
 		/// </summary>
 		/// <param name="multiples">List of values we have to find the multiples of</param>
 		/// <param name="maxValue">Maximum value for the iteration (not included)</param>
-		/// <returns></returns>
+		/// <returns>The sum of the values</returns>
 	    public static int Sum(List<int> multiples, int maxValue)
 	    {
 			//Initializing the result
@@ -25,9 +25,60 @@ namespace Code4Fun_Bellotti
 		    return result;
 	    }
 
+		/// <summary>
+		/// Unwraps a matrix and prints it in spiral order
+		/// </summary>
+		/// <param name="matrix">Matrix of elements to print</param>
+		/// <returns>A string representing the matrix unwrapped in spiral order</returns>
 	    public static string PrintSpiral(int[][] matrix)
 	    {
-		    return null;
+			if(matrix.Length == 0 || 
+				matrix.Length == 1 || 
+				matrix.First().Length == 0 || 
+				matrix.First().Length == 1)
+				throw new ArgumentException("Matrix not well formed");
+
+			var result = new List<int>();
+
+		    int startingRowIndex = 0, startingColumnIndex = 0;
+		    var endingRow = matrix.Length;
+		    var endingColumn = matrix.First().Length;
+ 
+			while (startingRowIndex < endingRow && startingColumnIndex < endingColumn)
+			{
+				/* Print the first row from the remaining rows */
+				int iterator;
+				for (iterator = startingColumnIndex; iterator < endingColumn; ++iterator)				
+					result.Add(matrix[startingRowIndex][iterator]);
+				
+				startingRowIndex++;
+ 
+				/* Print the last column from the remaining columns */
+				for (iterator = startingRowIndex; iterator < endingRow; ++iterator)				
+					result.Add(matrix[iterator][endingColumn-1]);
+				
+				endingColumn--;
+ 
+				/* Print the last row from the remaining rows */
+				if ( startingRowIndex < endingRow)
+				{
+					for (iterator = endingColumn-1; iterator >= startingColumnIndex; --iterator)					
+						result.Add(matrix[endingRow-1][iterator]);
+					
+					endingRow--;
+				}
+ 
+				/* Print the first column from the remaining columns */
+				if (startingColumnIndex >= endingColumn) 
+					continue;
+
+				for (iterator = endingRow-1; iterator >= startingRowIndex; --iterator)					
+					result.Add(matrix[iterator][startingColumnIndex]);
+					
+				startingColumnIndex++;
+			}
+
+		    return String.Join(", ", result);
 	    }
     }
 }
