ce781f174540f032c345aa12d52bcec1c4433319
Agonar87 <ste.bellotti@gmail.com>
2015-03-30 13:40:15 +0200

    Added Sum test and method

--- Code4Fun_Bellotti/Methods.cs
+++ Code4Fun_Bellotti/Methods.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Code4Fun_Bellotti
 {
-    public class Methods
+    public static class Methods
     {
+		/// <summary>
+		/// Calculates the sum of all the multiples
+		/// </summary>
+		/// <param name="multiples">List of values we have to find the multiples of</param>
+		/// <param name="maxValue">Maximum value for the iteration (not included)</param>
+		/// <returns></returns>
+	    public static int Sum(List<int> multiples, int maxValue)
+	    {
+			//Initializing the result
+		    var result = 0;
+
+			//Cicle which adds the number only if it is a multiple of a number in the list
+		    for (var i = 0; i < maxValue; i++)		    
+				result += multiples.Any(x => i % x == 0) ? i : 0;		    
+
+			//Returning result
+		    return result;
+	    }
     }
 }
--- Tests/UnitTest.cs
+++ Tests/UnitTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Code4Fun_Bellotti;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -9,6 +11,17 @@ namespace Tests
 		[TestMethod]
 		public void Test_CalculateSum()
 		{
+			//Creating the list of numbers for the multiples
+			var multiples = new List<int> {3, 5};
+
+			//Definition of the max value for the iteration
+			const int maxValue = 1000;
+
+			//Getting the result
+			var result = Methods.Sum(multiples, maxValue);
+
+			//Reporting the result
+			Console.Write("The sum of all the multiples of {0} below {1} (excluded) is: {2}", String.Join(" or ", multiples), maxValue, result);
 		}
 	}
 }
